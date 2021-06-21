3bb5d8b28b0da81aefa853c9cde5bc291033a667
unknown <deividalmeida365@gmail.com>
2021-06-21 15:17:06 -0300

    changes

---
+++ src/components/Button.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export function Button() {
+
+
+    const [counter, setCounter] = useState(0);
+
+
+
+
+function increment() {
+
+    setCounter(counter + 1);
+    
+}
+
+
+
+    return(
+        <button onClick={increment}>{counter}</button>
+    )
+
+}
+
